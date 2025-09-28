a981f881f9320434e9e979723cf7968c8aa5ea55
Glenn <glenn.liuchanghui@gmail.com>
2025-09-28 15:41:26 +0800

    added base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-hand-app/', // Replace 'my-react-app' with your repository name
 })
+
 
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
